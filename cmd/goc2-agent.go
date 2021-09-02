511aa464b2627fd403ffa5326d24d349348ed0d0
grines <grines@grines.com>
2021-09-01 20:36:56 -0700

    add linux

---
+++ cmd/goc2-agent.go
@@ -0,0 +1,23 @@
+package goc2agent
+
+import (
+	"flag"
+
+	"github.com/grines/goc2-agent/pkg/linux"
+)
+
+var (
+	typePtr string
+)
+
+//Start RedMap
+func Start() {
+	//flags
+	flag.StringVar(&typePtr, "type", "linux", "Build Type")
+	flag.Parse()
+
+	if typePtr == "linux" {
+		linux.Build()
+	}
+
+}
